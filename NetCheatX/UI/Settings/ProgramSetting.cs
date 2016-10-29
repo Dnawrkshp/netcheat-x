35110fafe3050ae05da96ce457add37e6c9b2645
Dan Gerendasy <werewu45@yahoo.com>
2016-10-28 18:03:53 -0700

    WIP Plugin development integration

---
+++ NetCheatX/UI/Settings/ProgramSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCheatX.UI.Settings
+{
+    public class ProgramSetting
+    {
+        public List<string> UpdateURLs { get; set; }
+
+        public List<string> PluginPaths { get; set; }
+
+        public ProgramSetting(string path)
+        {
+
+        }
+    }
+}
